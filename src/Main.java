21d707e8684db1ad56e83f28b2bdbd2743dbb107
Jona <jonam5623@gmail.com>
2026-06-28 22:52:51 -0500

    Agregar despedida personalizada

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         Saludo saludo = new Saludo();
         System.out.println(saludo.generarSaludo("Hola Arauz Mino"));
         System.out.println("Versión 1.0.0 lista para producción.");
+        Despedida despedida = new Despedida();
+        System.out.println(despedida.generarDespedida("Mundo"));
     }
 }
