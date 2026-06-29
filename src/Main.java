a58af48ab0e95865c1d03415cef7b5732b474b8e
Jona <jonam5623@gmail.com>
2026-06-28 22:48:08 -0500

    Agregar saludo personalizado

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,6 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main
-{
-    public static void main(String[] args)
-    {
-
-
+public class Main {
+    public static void main(String[] args) {
+        Saludo saludo = new Saludo();
+        System.out.println(saludo.generarSaludo("Mundo"));
     }
-
 }
