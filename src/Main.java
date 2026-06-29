3876be06c0cb60ace81bc94031eba3c1cc974146
Jona <jonam5623@gmail.com>
2026-06-28 22:53:23 -0500

    Preparar versión 1.1.0 con logo ASCII

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("***************");
+        System.out.println("* JAVA PROJECT *");
+        System.out.println("***************");
         Saludo saludo = new Saludo();
         System.out.println(saludo.generarSaludo("Hola Arauz Mino"));
         System.out.println("Versión 1.0.0 lista para producción.");
