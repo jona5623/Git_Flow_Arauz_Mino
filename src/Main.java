78edb08f97bbff01966c1b15345c151c1628a2a8
Jona <jonam5623@gmail.com>
2026-06-28 22:54:04 -0500

    Agregar saludo multilingüe (es/en/fr)

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,9 @@ public class Main {
         System.out.println("* JAVA PROJECT *");
         System.out.println("***************");
         Saludo saludo = new Saludo();
-        System.out.println(saludo.generarSaludo("Hola Arauz Mino"));
+        System.out.println(saludo.generarSaludo("Mundo", "en"));
+        System.out.println(saludo.generarSaludo("Mundo", "es"));
+        System.out.println(saludo.generarSaludo("Mundo", "fr"));
         System.out.println("Versión 1.0.0 lista para producción.");
         Despedida despedida = new Despedida();
         System.out.println(despedida.generarDespedida("Mundo"));
